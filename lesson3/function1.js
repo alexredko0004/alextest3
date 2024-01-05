c26cb6252897936e07076d0f847255bb0aa1a780
alexredko0004 <alexredko0004@gmail.com>
2024-01-06 00:01:24 +0200

    added comments and discriminant function

--- lesson3/function1.js
+++ lesson3/function1.js
@@ -1,19 +1,19 @@
+//Square number
 function square(number) {
     return number*number;
 }
 let x2 = square (8);
 console.log (x2);
 
-let x = 10;
-let y = 3;
-
-// console.log (x + y);
-// console.log (y - x);
-// console.log (x * y);
-// console.log (x / y);
-// console.log (x % y);
-// console.log (x ** y);
+// Increment number
 function increment (a) {
     return ++a
 }
 console.log(increment(10));
+
+// Discriminant
+function disc (a,b,c) {
+    let d = b*b-4*a*c;
+    return d;
+}
+console.log ('Your discriminant is '+ disc(14,20,5))
