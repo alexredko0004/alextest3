7c278718c498feefbcd33058bd53facd00e00623
alexredko0004 <alexredko0004@gmail.com>
2024-01-05 20:37:41 +0200

    added file with variables

--- lesson3/function1.js
+++ lesson3/function1.js
@@ -3,3 +3,17 @@ function square(number) {
 }
 let x2 = square (8);
 console.log (x2);
+
+let x = 10;
+let y = 3;
+
+// console.log (x + y);
+// console.log (y - x);
+// console.log (x * y);
+// console.log (x / y);
+// console.log (x % y);
+// console.log (x ** y);
+function increment (a) {
+    return ++a
+}
+console.log(increment(10));
---
+++ lesson3/variables.js
@@ -0,0 +1,25 @@
+let name;
+name = 'Alex';
+console.log (name);
+
+const jobProfile = 'QA';
+console.log ('Hello '+ name + ' '+jobProfile);
+
+const permission = 'Full';
+let id = 84398785234324357;
+let isApproved = true;
+const truth = 2<1;
+let car;
+let age = null;
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    region: 38583
+}
+console.log (typeof permission, permission);
+console.log (typeof id, id);
+console.log (typeof isApproved, isApproved);
+console.log (typeof truth, truth);
+console.log (typeof car, car);
+console.log (typeof age, age);
+console.log (typeof person, person);
